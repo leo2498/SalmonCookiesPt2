ee9435cd2bec45cee24278c7681719024a0be863
Leo Juarez <juarezleo9@gmail.com>
2019-01-16 08:57:12 -0800

    Adds minor updates

--- app.js
+++ app.js
@@ -62,20 +62,20 @@ var cookieSales
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var Table = document.getElementById('Cookie table');
 
-  new Store('1st & Pike', 23, 65, 6.3),
-  new Store('SeaTac Airport', 3, 24, 1.2),
-  new Store('Seattle Center', 11, 38, 3.7),
-  new Store('Capitol Hill', 20, 38, 2.3),
-  new Store('Alki', 2, 16, 4.6),
+  new Store('1st & Pike', 23, 65, 6.3)
+  new Store('SeaTac Airport', 3, 24, 1.2)
+  new Store('Seattle Center', 11, 38, 3.7)
+  new Store('Capitol Hill', 20, 38, 2.3)
+  new Store('Alki', 2, 16, 4.6)
 
 function Store(store, minCust, maxCust, avgCookie) {
   this.storeName = store;
-  this.minCust = minCust,
-  this.maxCust = maxCust,
-  this.avgCookie = avgCookie,
+  this.minCust = minCust
+  this.maxCust = maxCust
+  this.avgCookie = avgCookie
   this.storeCookiePerHour = [];
   this.custPerHour = [];
-  this.totalDailyCookies = 0;
+  this.totalDailyCookies = 0
   cookieSales.push(this)
 }
 
